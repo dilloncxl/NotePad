9947b431c7033fe16d9149bc3be0edb5b890dfb2
dilloncxl <dilloncxl@gmail.com>
2014-07-12 10:41:06 +0800

    Initial Commit

---
+++ NotePad/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NotePad
+//
+//  Created by chenxiaolong on 14-7-12.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
